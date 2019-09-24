66e381550bd2160ede7fe10bfb8b610b3c67c479
anikamorris <anikamorris11@gmail.com>
2019-09-24 15:13:28 -0700

    class Hero functional

--- superheroes.py
+++ superheroes.py
@@ -15,7 +15,7 @@ class Ability:
         the initialized max_damage strength. ''' 
         return random.randint(0, self.max_damage)
 
-class Armour:
+class Armor:
 
     def __init__(self, name, max_block):
         ''' Create Instance Variables:
@@ -57,26 +57,61 @@ class Hero:
         '''Calculate the total damage from all ability attacks.
           return: total: Integer
         '''
+        damage = 0
         for ability in self.abilities:
-            print(ability.attack())
+            damage += ability.attack()
+        return damage
 
-    def defend(self, incoming_damage):
-        pass
+    def add_armor(self, armor):
+        '''Add armor to self.armors
+            armor: Armor object
+        '''
+        self.armors.append(armor)
+
+    def defend(self):
+        ''' Runs 'block' method on each armor
+            Returns sum of all blocks 
+        '''
+        sum = 0
+        for armor in self.armors:
+            sum += armor.block()
+        return sum
 
     def take_damage(self, damage):
-        pass
+        '''Updates self.current_health to reflect damage minus defense'''
+        defense = self.defend()
+        damage = damage - defense
+        self.current_health -= damage
 
     def is_alive(self):
-        pass
+        return self.current_health > 0
 
     def fight(self, opponent):
-        pass
+        while self.is_alive() and opponent.is_alive():
+            if not self.abilities and not opponent.abilities:
+                print("Draw")
+            else:
+                damage = self.attack()
+                print(damage)
+                opponent.take_damage(damage)
+                damage = opponent.attack()
+                print(damage)
+                self.take_damage(damage)
 
+        if self.is_alive():
+            print(self.name + ' won!')
+        else:
+            print(opponent.name + ' won!')
+            
 if __name__=="__main__":
-    ability = Ability('Great Debugging', 50)
-    ability2 = Ability('High Jumps', 90)
-    my_hero = Hero('Grace Hopper', 200)
-    my_hero.add_ability(ability)
-    my_hero.add_ability(ability2)
-    print(my_hero.attack())
-
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+    hero1.fight(hero2)
