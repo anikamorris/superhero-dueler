a82f60b8b3e4f6748a3f3db3af9efdb09b8d4134
anikamorris <anikamorris11@gmail.com>
2019-09-25 16:52:24 -0700

    passes all tests/page 4 complete

--- superheroes.py
+++ superheroes.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Ability:
 
     def __init__(self, name, max_damage):
@@ -12,8 +13,9 @@ class Ability:
 
     def attack(self):
         ''' Return a random value between 0 and 
-        the initialized max_damage strength. ''' 
+        the initialized max_damage strength. '''
         return random.randint(0, self.max_damage)
+
 
 class Armor:
 
@@ -27,11 +29,18 @@ class Armor:
 
     def block(self):
         ''' Return a random value between 0 and 
-        the initialized max_block strength. ''' 
+        the initialized max_block strength. '''
         return random.randint(0, self.max_block)
 
+
+class Weapon(Ability):
+
+    def attack(self):
+        return random.randint(self.max_damage//2, self.max_damage)
+
+
 class Hero:
-    
+
     def __init__(self, name, starting_health=100):
         '''Create Instance Variables:
         name: String
@@ -49,10 +58,12 @@ class Hero:
         self.abilities = []
         self.armors = []
         self.current_health = self.starting_health
+        self.deaths = 0
+        self.kills = 0
 
     def add_ability(self, ability):
         self.abilities.append(ability)
-    
+
     def attack(self):
         '''Calculate the total damage from all ability attacks.
           return: total: Integer
@@ -86,6 +97,14 @@ class Hero:
     def is_alive(self):
         return self.current_health > 0
 
+    def add_kill(self, num_kills):
+        ''' Update kills with num_kills'''
+        self.kills += num_kills
+
+    def add_death(self, num_deaths):
+        ''' Update deaths with num_deaths '''
+        self.deaths += num_deaths
+
     def fight(self, opponent):
         while self.is_alive() and opponent.is_alive():
             if not self.abilities and not opponent.abilities:
@@ -100,18 +119,73 @@ class Hero:
 
         if self.is_alive():
             print(self.name + ' won!')
+            self.add_kill(1)
+            opponent.add_death(1)
         else:
             print(opponent.name + ' won!')
-            
-if __name__=="__main__":
+            opponent.add_kill(1)
+            self.add_death(1)
+
+
+class Team:
+    def __init__(self, name):
+        '''Instatiate instance properties
+
+            name: String
+            heroes: List of Hero objects
+        '''
+        self.name = name
+        self.heroes = []
+
+    def add_hero(self, hero):
+        self.heroes.append(hero)
+        print(self.heroes[len(self.heroes)-1].name)
+
+    def remove_hero(self, hero_name):
+        '''Remove hero from heroes list given a name
+            If hero isn't found, return 0
+        '''
+        for hero in self.heroes:
+            if len(self.heroes) > 0 and hero.name == hero_name:
+                self.heroes.remove(hero)
+                return 0
+        else:
+            return 0
+
+    def view_all_heroes(self):
+        if len(self.heroes) > 0:
+            for hero in self.heroes:
+                print(hero.name)
+        else:
+            print("There are no heroes on this team yet.")
+
+    def attack(self, other_team):
+        index = random.randint(0, len(self.heroes)-1)
+        my_hero = self.heroes[index]
+        index = random.randint(0, len(other_team.heroes)-1)
+        my_opponent = other_team.heroes[index]
+        my_hero.fight(my_opponent)
+
+    def revive_heroes(self, health=100):
+        for hero in self.heroes:
+            hero.current_health = health
+    
+    def stats(self):
+        for hero in self.heroes:
+            if hero.deaths > 0:
+                ratio = hero.kills//hero.deaths
+                print(hero.name + ": " + ratio)
+            else:
+                print(hero.name + ": " + hero.kills)
+
+
+if __name__ == "__main__":
     hero1 = Hero("Wonder Woman")
     hero2 = Hero("Dumbledore")
-    ability1 = Ability("Super Speed", 300)
-    ability2 = Ability("Super Eyes", 130)
-    ability3 = Ability("Wizard Wand", 80)
-    ability4 = Ability("Wizard Beard", 20)
-    hero1.add_ability(ability1)
-    hero1.add_ability(ability2)
-    hero2.add_ability(ability3)
-    hero2.add_ability(ability4)
-    hero1.fight(hero2)
+    team1 = Team("1")
+    team1.add_hero(hero1)
+    team1.add_hero(hero2)
+    team1.remove_hero(hero2)
+    team1.view_all_heroes()
+    team1.remove_hero(hero1)
+    
